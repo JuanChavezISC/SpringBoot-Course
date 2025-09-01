d459ddebdb17ebfee7d421c5b0292458050e02c6
Juan Carlos Chavez <juanchavezsn@gmail.com>
2025-09-01 13:50:59 -0600

    FirstCommit Project

---
+++ restfulApi/src/main/java/com/restfulapi/entity/Greeting.java
@@ -0,0 +1,14 @@
+package com.restfulapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greeting {
+
+	private long id;
+	private String content;
+}
